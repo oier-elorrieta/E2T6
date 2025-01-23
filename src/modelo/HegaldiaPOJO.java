49e07dc34932e0998a6df93856ef15dd05648382
AitorZuloaga <Aitor.ZuloagaLe@elorrieta-errekamari.com>
2025-01-23 12:29:16 +0100

    HegaldiaPOJO eguneratua, joanetorriren atributuekin

--- src/modelo/HegaldiaPOJO.java
+++ src/modelo/HegaldiaPOJO.java
@@ -13,9 +13,19 @@ public class HegaldiaPOJO {
 	private double prezioa;
 	private Date irteera;
 	private Date iraupena;
+	private String jatorria_buelta;
+	private String helmuga_buelta;
+	private Date data_buelta;
+	private String kod_buelta;
+	private String aerolinea_buelta;
+	private double prezioa_buelta;
+	private Date irteera_buelta;
+	private Date iraupena_buelta;
+	
 	
 	public HegaldiaPOJO(String izena, String jatorria, String helmuga, Date data, String kod, String aerolinea,
 			double prezioa, Date irteera, Date iraupena) {
+		
 		this.izena = izena;
 		this.jatorria = jatorria;
 		this.helmuga = helmuga;
@@ -25,6 +35,30 @@ public class HegaldiaPOJO {
 		this.prezioa = prezioa;
 		this.irteera = irteera;
 		this.iraupena = iraupena;
+	}
+
+	public HegaldiaPOJO(String izena, String jatorria, String helmuga, Date data, String kod, String aerolinea,
+			double prezioa, Date irteera, Date iraupena, String jatorria_buelta, String helmuga_buelta,
+			Date data_buelta, String kod_buelta, String aerolinea_buelta, double prezioa_buelta, Date irteera_buelta,
+			Date iraupena_buelta) {
+		
+		this.izena = izena;
+		this.jatorria = jatorria;
+		this.helmuga = helmuga;
+		this.data = data;
+		this.kod = kod;
+		this.aerolinea = aerolinea;
+		this.prezioa = prezioa;
+		this.irteera = irteera;
+		this.iraupena = iraupena;
+		this.jatorria_buelta = jatorria_buelta;
+		this.helmuga_buelta = helmuga_buelta;
+		this.data_buelta = data_buelta;
+		this.kod_buelta = kod_buelta;
+		this.aerolinea_buelta = aerolinea_buelta;
+		this.prezioa_buelta = prezioa_buelta;
+		this.irteera_buelta = irteera_buelta;
+		this.iraupena_buelta = iraupena_buelta;
 	}
 
 	public String getIzena() {
@@ -105,7 +139,69 @@ public class HegaldiaPOJO {
 				+ ", kod=" + kod + ", aerolinea=" + aerolinea + ", prezioa=" + prezioa + ", irteera=" + irteera
 				+ ", iraupena=" + iraupena;
 	}
-	
-	
+
+	public String getJatorria_buelta() {
+		return jatorria_buelta;
+	}
+
+	public void setJatorria_buelta(String jatorria_buelta) {
+		this.jatorria_buelta = jatorria_buelta;
+	}
+
+	public String getHelmuga_buelta() {
+		return helmuga_buelta;
+	}
+
+	public void setHelmuga_buelta(String helmuga_buelta) {
+		this.helmuga_buelta = helmuga_buelta;
+	}
+
+	public Date getData_buelta() {
+		return data_buelta;
+	}
+
+	public void setData_buelta(Date data_buelta) {
+		this.data_buelta = data_buelta;
+	}
+
+	public String getKod_buelta() {
+		return kod_buelta;
+	}
+
+	public void setKod_buelta(String kod_buelta) {
+		this.kod_buelta = kod_buelta;
+	}
+
+	public String getAerolinea_buelta() {
+		return aerolinea_buelta;
+	}
+
+	public void setAerolinea_buelta(String aerolinea_buelta) {
+		this.aerolinea_buelta = aerolinea_buelta;
+	}
+
+	public double getPrezioa_buelta() {
+		return prezioa_buelta;
+	}
+
+	public void setPrezioa_buelta(double prezioa_buelta) {
+		this.prezioa_buelta = prezioa_buelta;
+	}
+
+	public Date getIrteera_buelta() {
+		return irteera_buelta;
+	}
+
+	public void setIrteera_buelta(Date irteera_buelta) {
+		this.irteera_buelta = irteera_buelta;
+	}
+
+	public Date getIraupena_buelta() {
+		return iraupena_buelta;
+	}
+
+	public void setIraupena_buelta(Date iraupena_buelta) {
+		this.iraupena_buelta = iraupena_buelta;
+	}	
 
 }
