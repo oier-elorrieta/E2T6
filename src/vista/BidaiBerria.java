227a2e4e6c7fa8680fe40008d2dd0c9ccb4cb55c
Joseba Murua <joseba.muruabi@elorrieta-errekamari.com>
2025-01-29 13:10:19 +0100

    Herraldeak aukera gehitu

--- src/vista/BidaiBerria.java
+++ src/vista/BidaiBerria.java
@@ -43,7 +43,7 @@ public class BidaiBerria extends JFrame {
 	 */
 	public BidaiBerria() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 430);
+		setBounds(100, 100, 500, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -51,71 +51,79 @@ public class BidaiBerria extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Bidaiaren Izena:");
-		lblNewLabel.setBounds(27, 39, 110, 14);
+		lblNewLabel.setBounds(27, 40, 110, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblEvento = new JLabel("Bidaia Mota:");
-		lblEvento.setBounds(27, 66, 110, 14);
+		lblEvento.setBounds(27, 65, 110, 14);
 		contentPane.add(lblEvento);
 		
 		JLabel lblBidaiHasiera = new JLabel("Bidai Hasiera:");
-		lblBidaiHasiera.setBounds(27, 91, 110, 14);
+		lblBidaiHasiera.setBounds(27, 90, 110, 14);
 		contentPane.add(lblBidaiHasiera);
 		
 		JLabel lblBiadiAmaiera = new JLabel("Biadi Amaiera:");
-		lblBiadiAmaiera.setBounds(27, 116, 135, 14);
+		lblBiadiAmaiera.setBounds(27, 115, 135, 14);
 		contentPane.add(lblBiadiAmaiera);
 		
 		JLabel lblEgunak = new JLabel("Egunak:");
-		lblEgunak.setBounds(27, 141, 135, 14);
+		lblEgunak.setBounds(27, 140, 135, 14);
 		contentPane.add(lblEgunak);
 		
 		textEizena = new JTextField();
-		textEizena.setBounds(189, 39, 150, 20);
+		textEizena.setBounds(189, 40, 150, 20);
 		contentPane.add(textEizena);
 		textEizena.setColumns(10);
 		
 		textEgunak = new JTextField();
 		textEgunak.setColumns(10);
-		textEgunak.setBounds(189, 141, 89, 20);
+		textEgunak.setBounds(189, 140, 89, 20);
 		contentPane.add(textEgunak);
 		
 		JComboBox<String> comboBoxEmota = new JComboBox<>();
-		comboBoxEmota.setBounds(189, 66, 150, 20);
+		comboBoxEmota.setBounds(189, 65, 150, 20);
 		contentPane.add(comboBoxEmota);
 		
 		JTextArea textArea = new JTextArea();
 		textArea.setEditable(false);
-		textArea.setBounds(189, 172, 200, 63);
+		textArea.setBounds(189, 196, 200, 63);
 		contentPane.add(textArea);
 		
 		JLabel lblDescribapena = new JLabel("Describapena:");
-		lblDescribapena.setBounds(27, 192, 147, 14);
+		lblDescribapena.setBounds(27, 216, 147, 14);
 		contentPane.add(lblDescribapena);
 		
 		JLabel lblNewLabel_1 = new JLabel("Sartu Gabeko Zerbitzuak");
-		lblNewLabel_1.setBounds(27, 269, 150, 14);
+		lblNewLabel_1.setBounds(27, 293, 150, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		JTextArea textSartuGabekoZerbitzuak = new JTextArea();
 		textSartuGabekoZerbitzuak.setEditable(false);
-		textSartuGabekoZerbitzuak.setBounds(189, 246, 200, 63);
+		textSartuGabekoZerbitzuak.setBounds(189, 270, 200, 63);
 		contentPane.add(textSartuGabekoZerbitzuak);
 		
 		JDateChooser BidaiaHasiera = new JDateChooser();
-		BidaiaHasiera.setBounds(189,91,174,20);
+		BidaiaHasiera.setBounds(189,90,174,20);
 		contentPane.add(BidaiaHasiera);
 		
 		JDateChooser BidaiaAmaiera = new JDateChooser();
-		BidaiaAmaiera.setBounds(189, 116, 174, 20);
+		BidaiaAmaiera.setBounds(189, 115, 174, 20);
 		contentPane.add(BidaiaAmaiera);
 		
 		JButton btnGorde = new JButton("Gorde");
-		btnGorde.setBounds(27, 345, 89, 23);
+		btnGorde.setBounds(27, 369, 89, 23);
 		contentPane.add(btnGorde);
 		
 		JButton btnItzuli = new JButton("Itzuli");
-		btnItzuli.setBounds(206, 345, 89, 23);
+		btnItzuli.setBounds(189, 369, 89, 23);
 		contentPane.add(btnItzuli);
+		
+		JLabel lblHerrialde = new JLabel("Herrialde:");
+		lblHerrialde.setBounds(27, 165, 135, 14);
+		contentPane.add(lblHerrialde);
+		
+		JComboBox<String> comboBoxHerralde = new JComboBox<String>();
+		comboBoxHerralde.setBounds(189, 165, 150, 20);
+		contentPane.add(comboBoxHerralde);
 	}
 }
